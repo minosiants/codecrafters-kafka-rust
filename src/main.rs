1cc9d2b09fdb9e29236bdac61fce043fe7af96c2
minosiants <kaspar.ru@gmail.com>
2024-12-05 10:45:16 +0300

    Parce correlation id

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@
 
 use std::io::{Read, Write};
 use std::net::TcpListener;
-use bytes::{BufMut, Bytes, Buf};
+use bytes::{BufMut, Bytes};
 
 
 struct Header {
-    request_api_key:i16,
-    request_api_version:i16,
-    correlation_id:i32,
+    request_api_key: i16,
+    request_api_version: i16,
+    correlation_id: i32,
 }
 impl Header {
-    fn new(request_api_key:i16, request_api_version:i16, correlation_id:i32)-> Self {
+    fn new(request_api_key: i16, request_api_version: i16, correlation_id: i32) -> Self {
         Self {
             request_api_key,
             request_api_version,
@@ -29,7 +29,7 @@ impl Into<Vec<u8>> for Header {
     }
 }
 
-impl From<&[u8]>  for Header{
+impl From<&[u8]> for Header {
     fn from(bytes: &[u8]) -> Self {
         let request_api_key = i16::from_be_bytes(bytes[0..2].try_into().unwrap());
         let request_api_version = i16::from_be_bytes(bytes[2..4].try_into().unwrap());
@@ -38,14 +38,14 @@ impl From<&[u8]>  for Header{
     }
 }
 struct Request {
-    message_size:i32,
-    header:Header,
+    message_size: i32,
+    header: Header,
 }
 impl Request {
-    fn new(message_size:i32, header:Header) -> Self {
-        Self{
+    fn new(message_size: i32, header: Header) -> Self {
+        Self {
             message_size,
-            header
+            header,
         }
     }
 }
@@ -53,7 +53,7 @@ impl Into<Vec<u8>> for Request {
     fn into(self) -> Vec<u8> {
         let mut bytes = Vec::new();
         bytes.extend(self.message_size.to_be_bytes());
-        let header:Vec<u8> = self.header.into();
+        let header: Vec<u8> = self.header.into();
         bytes.extend_from_slice(header.as_slice());
         bytes
     }
@@ -62,7 +62,7 @@ impl Into<Vec<u8>> for Request {
 impl From<&[u8]> for Request {
     fn from(bytes: &[u8]) -> Self {
         let correlation_id = i32::from_be_bytes(bytes[0..4].try_into().unwrap());
-        let header:Header  = Header::from(&bytes[4..12]);
+        let header: Header = Header::from(&bytes[4..12]);
         Request::new(correlation_id, header)
     }
 }
@@ -77,21 +77,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let mut len = [0; 4];
-                stream.read_exact(&mut len).unwrap();
-                let len = i32::from_be_bytes(len) as usize;
-                let mut request = vec![0; len];
-                stream.read_exact(&mut request).unwrap();
-                let mut request = request.as_slice();
-                let _request_api_key = request.get_i16();
-                let _request_api_version = request.get_i16();
-                let correlation_id = request.get_i32();
-                let mut response = Vec::with_capacity(8);
-                response.put_i32(0);
-                response.put_i32(correlation_id);
-                stream.write_all(&response).unwrap();
+                println!("accepted new connection");
+                /* let message_size:i32 = 35;
+                 let request_api_key:i16 = 18;
+                 let request_api_version:i16 = 4;
+                 let correletion_id:i32 = 1870644833;*/
+                let mut buffer = [0; 12];
+                stream.read_exact(&mut buffer).unwrap();
 
-
+                let req: Request = Request::from(buffer.as_slice());
+                let mut resp: Vec<u8> = Vec::with_capacity(8);
+                resp.put_i32(0);
+                resp.extend_from_slice(req.header.correlation_id.to_be_bytes().as_slice());
+                println!("correlationId {}", req.header.correlation_id);
+                stream.write_all(resp.as_slice()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
